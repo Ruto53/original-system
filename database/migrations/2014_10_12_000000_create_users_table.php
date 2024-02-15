d9d97d18649312e2d108ae5683b0023b625085f4
Mona Lisa <ayazun0219@gmail.com>
2024-02-15 20:12:17 +0900

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('use_id')->unsigned();
-            $table->string('use_name');
+            $table->id('id')->unsigned();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -26,7 +26,7 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
+     *VS
      * @return void
      */
     public function down()
